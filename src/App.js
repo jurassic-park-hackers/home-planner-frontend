2cf7b17e887cf46fda82b0d1ba1663690598fcad
Muriel Silveira <murielkong@gmail.com>
2018-05-22 22:07:17 +0200

    feature(ui): Install material-ui components and use a sample app
    https://material-ui.com/

--- src/App.js
+++ src/App.js
@@ -1,13 +1,77 @@
-import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Drawer from '@material-ui/core/Drawer'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import List from '@material-ui/core/List'
+import Typography from '@material-ui/core/Typography'
+import Divider from '@material-ui/core/Divider'
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import DoneAll from '@material-ui/icons/DoneAll'
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        ôe!
-      </div>
-    )
-  }
+const drawerWidth = 240
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  drawerPaper: {
+    position: 'relative',
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+    minWidth: 0, // So the Typography noWrap works
+  },
+  toolbar: theme.mixins.toolbar,
+})
+
+function App({ classes }) {
+  return (
+    <div className={classes.root}>
+      <AppBar position="absolute" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="title" color="inherit" noWrap>
+            Home Planner
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        classes={{ paper: classes.drawerPaper }}
+      >
+        <div className={classes.toolbar} />
+        <List>
+          <ListItem button>
+            <ListItemIcon>
+              <DoneAll />
+            </ListItemIcon>
+            <ListItemText primary="Tasks" />
+          </ListItem>
+        </List>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Typography noWrap>{'You think water moves fast? You should see ice.'}</Typography>
+      </main>
+    </div>
+  )
 }
 
-export default App
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(App)
