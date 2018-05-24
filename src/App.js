6a416b2dbcd49c07475993f838cc85066e2854be
Muriel Silveira <murielkong@gmail.com>
2018-05-24 22:08:06 +0200

    fix: Remove unused var that breakes the production build
    This will be checked by eslint on the CI soon.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,9 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import List from '@material-ui/core/List'
 import Typography from '@material-ui/core/Typography'
-import Divider from '@material-ui/core/Divider'
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
 import DoneAll from '@material-ui/icons/DoneAll'
 
 const drawerWidth = 240
